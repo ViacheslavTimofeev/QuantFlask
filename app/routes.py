a6ed719e99099c0d4accdc90ed9bb3f611a73a5d
k1b3r74rd <kibertard@gmail.com>
2020-12-14 15:14:22 +0700

    14.12 some pages

--- app/routes.py
+++ app/routes.py
@@ -4,38 +4,22 @@ from app.forms import LoginForm
 
 
 @app.route('/')
-@app.route('/index')
-def index():
-    user = {'username': 'Miguel'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+@app.route('/home')
+def home():
+    return render_template('home.html', title='Home')
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
-    return render_template('login.html',  title='Sign In', form=form)
+@app.route('/about')
+def about():
+    return render_template('about.html',  title='About')
 
 
-@app.route('/personal')
-def personal():
-    return render_template('personal.html')
+@app.route('/certificate')
+def certificate():
+    return render_template('certificate.html')
 
 
-@app.route('/filler')
-def filler():
-    return render_template('filler.html')
+@app.route('/coming')
+def coming():
+    return render_template('coming.html')
 
